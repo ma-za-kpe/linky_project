559bbd90f8bdec5025a80b41b16973a837b9c6aa
ma-za-kpe <makpalyy@gmail.com>
2024-02-14 01:40:29 +0000

    add viewset comments uing doc string

--- profiles/views.py
+++ profiles/views.py
@@ -3,5 +3,26 @@ from .models import Profile
 from .serializers import ProfileSerializer
 
 class ProfileViewSet(viewsets.ModelViewSet):
+    """
+    A viewset that provides CRUD operations for profiles.
+
+    retrieve:
+    Return a specific profile instance.
+
+    list:
+    Return a list of all profiles.
+
+    create:
+    Create a new profile instance.
+
+    update:
+    Update an existing profile instance.
+
+    partial_update:
+    Partially update an existing profile instance.
+
+    destroy:
+    Delete a profile instance.
+    """
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
--- projects/views.py
+++ projects/views.py
@@ -1,15 +1,53 @@
-from django.shortcuts import render
-
-# Create your views here.
 from rest_framework import viewsets
 from .models import Project, Link
 from .serializers import ProjectSerializer, LinkSerializer
-    
+
 class ProjectsViewSet(viewsets.ModelViewSet):
+    """
+    A viewset that provides CRUD operations for projects.
+
+    retrieve:
+    Return a specific project instance.
+
+    list:
+    Return a list of all projects.
+
+    create:
+    Create a new project instance.
+
+    update:
+    Update an existing project instance.
+
+    partial_update:
+    Partially update an existing project instance.
+
+    destroy:
+    Delete a project instance.
+    """
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
-    
-    
+
 class LinkViewSet(viewsets.ModelViewSet):
+    """
+    A viewset that provides CRUD operations for links.
+
+    retrieve:
+    Return a specific link instance.
+
+    list:
+    Return a list of all links.
+
+    create:
+    Create a new link instance.
+
+    update:
+    Update an existing link instance.
+
+    partial_update:
+    Partially update an existing link instance.
+
+    destroy:
+    Delete a link instance.
+    """
     queryset = Link.objects.all()
     serializer_class = LinkSerializer
