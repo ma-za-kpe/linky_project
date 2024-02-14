42fd233c6c8a8e86601aa48be0ea8cb9e6bbde27
ma-za-kpe <makpalyy@gmail.com>
2024-02-14 00:01:12 +0000

    add api auth url

--- linky_api/settings.py
+++ linky_api/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        # 'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 }
 
