667bc99e18a7cb13b1a91d7d142457b2ba422349
ma-za-kpe <makpalyy@gmail.com>
2024-02-17 14:01:34 +0000

    update allowed hosts in settings file

--- linky_api/settings.py
+++ linky_api/settings.py
@@ -32,7 +32,13 @@ SECRET_KEY =  os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'linky-3il1.onrender.com'
+]
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
