bbf16bb4177b8d9ccedb4a870389b33d4304b523
Paulo Ouana <paulocouana.pauloc.ouana@gmail.com>
2024-03-04 14:03:45 +0000

    Fixing in the read me files

--- tags/urls.py
+++ tags/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from .views import TagList, TagDetail
 
 urlpatterns = [
-    path('/', TagDetail.as_view()),
+
+    path('', TagDetail.as_view()),  # While trying to create a new super_user I needed to remove the slash because it was throwing the following WARNINGS:
+    # ?: (urls.W002) Your URL pattern '/' has a route beginning with a '/'. Remove this slash as it is unnecessary. 
+    # If this pattern is targeted in an include(), ensure the include() pattern has a trailing '/'.
     path('', TagList.as_view()),
 ]
